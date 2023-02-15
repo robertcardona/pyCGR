b8f8104525699972437a363144dbcf228c39270b
Juan <juan.fraire@inria.fr>
2023-02-15 12:47:49 -0300

    Updated for rio 2023

--- py_cgr/rio-2023.py
+++ py_cgr/rio-2023.py
@@ -1,6 +1,8 @@
 import sys
 from py_cgr_lib.py_cgr_lib import cgr_yen
 from py_cgr_lib.py_cgr_lib import cp_load
+from py_cgr_lib.py_cgr_lib import fwd_candidate
+from py_cgr_lib.py_cgr_lib import Bundle
 
 source = 1      # source node A
 destination = 5 # destination node E
@@ -16,3 +18,9 @@ for contact in contact_plan:
 routes = cgr_yen(source, destination, curr_time, contact_plan, 10)
 for route in routes:
     print(route)
+
+print("---forward---")
+excluded_nodes = []
+bundle = Bundle(src=1, dst=5, size=1, deadline=60, priority=0)
+candidate_routes = fwd_candidate(curr_time, source, contact_plan, bundle, routes, excluded_nodes)
+print(candidate_routes[0])
