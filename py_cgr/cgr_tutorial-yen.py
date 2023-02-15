b6347f9aa48e986c59628b70f6b11b9ec8a54d8a
Juan <juan.fraire@inria.fr>
2023-02-15 14:58:26 -0300

    Updated Yen's example

--- py_cgr/cgr_tutorial-yen.py
+++ py_cgr/cgr_tutorial-yen.py
@@ -12,9 +12,6 @@ contact_plan = cp_load('./contact_plans/cgr_tutorial.txt', 5000)
 print(contact_plan)
 
 print("---yen---")
-for contact in contact_plan:
-    contact.clear_dijkstra_working_area()
-    contact.clear_management_working_area()
 routes = cgr_yen(source, destination, curr_time, contact_plan, 10)
 for route in routes:
     print(route)
@@ -24,3 +21,7 @@ excluded_nodes = []
 bundle = Bundle(src=1, dst=5, size=1, deadline=60, priority=0)
 candidate_routes = fwd_candidate(curr_time, source, contact_plan, bundle, routes, excluded_nodes)
 print(candidate_routes[0])
+
+
+
+
